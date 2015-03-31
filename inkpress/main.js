89998a5f21d3c20f1ea2ffe566061c936aa8e847
luciac <luciaconchello@yahoo.com>
2015-03-30 21:35:52 -0400

    WHAT ARE THE BABAIES DOING eating the cranberry people

--- inkpress/main.js
+++ inkpress/main.js
@@ -7,3 +7,7 @@ $(".menubotton").on("click",function(e){
 $(".menubotton").on("click",function(e){
 	$("#topbar").toggleClass("open");
 	});
+	
+$(".menubutton").on("click",function(e){
+	$("#topbar").toggleClass("open");
+	});
