c9cede06e4c6999d3489d9223e006e687678bd52
luciac <luciaconchello@giggle-water-buffalo.local>
2017-07-18 00:12:40 -0400

    boomcannons
    i should go to bed.
    also learn javascript

---
+++ randomize/brotatochip.js
@@ -0,0 +1,66 @@
+
+$(".scrolling").scrollFlight();
+$("#trigger").scrollFlight();
+$("#two").scrollFlight();
+$("#three").scrollFlight();
+$("#four").scrollFlight();
+
+
+/*
+$(".scrolling").on("arrived",function(e) {
+	$("#dacat").addClass("pop-up");
+	});
+*/
+
+/*
+$("#trigger").on("arrived",function(e) {
+	$("#dacat").addClass("riseup");
+});
+
+$("#three").onclick(
+	function() {
+		var threeflip = Math.random();
+if (threeflip <0.34){
+    threeflip = "3";
+    $("#trois").removeClass("hidden");
+}else if(threeflip <=0.67){
+    threeflip = "1";
+    $("#un").removeClass("hidden");
+}
+else{
+    threeflip = "2";
+    $("#deux").removeClass("hidden");
+};
+	});
+*/
+
+
+
+
+
+
+
+/*
+Math.floor(Math.random()*10)
+
+$("#dacat").hover(
+	function() {
+		$("#birthday").addClass("fade-in");
+		$("#head").addClass("balooned");
+		$(".lefti").addClass("widened");
+		$(".righti").addClass("widened");
+		$(".glimmer").addClass("widened");
+		$("#nose").addClass("move");
+		$(".ear").addClass("dissapear")
+		$(".down").addClass("moveit");
+		$(".trileft").addClass("moveit");
+		$(".triright").addClass("moveit");
+		$("#loon").addClass("descend");
+		$("#dacat").addClass("flyaway");
+		$("#raining").addClass("makeitrain");
+		$("#raining2").addClass("makeitrain");
+		$("#cover").addClass("goaway");
+		$("#instruct").addClass("fade-out");
+	});
+*/
+	
