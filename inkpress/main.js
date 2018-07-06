f097e836eef172b162328bb90a0369ff962afcac
luciac <luciaconchello@giggle-water-buffalo.local>
2018-07-06 00:27:29 -0400

    I deleted lots.
    WHY IS NOTHING SHOWING UP SONNNNN?!

--- inkpress/main.js
+++
@@ -1,9 +0,0 @@
-/*
-$(".menubotton").on("click",function(e){
-	$("#topbar").css({'height': 130 + "px"}) ;
-	})
-*/
-
-$(".menubotton").on("click",function(e){
-	$("#topbar").toggleClass("open");
-	});
